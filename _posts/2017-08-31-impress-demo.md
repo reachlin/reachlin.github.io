5c3720cdd04c25ad048e657f0ec55f46d8986562
reachlin <reachlin@gmail.com>
2017-09-01 14:57:43 +0800

    error

--- _posts/2017-08-31-impress-demo.md
+++ _posts/2017-08-31-impress-demo.md
@@ -3,15 +3,15 @@ title: Impress Demo.
 layout: impress
 ---
 
-# Page 1 |step,850,3000,90,5
+# Page 1 ::step,850,3000,90,5
 section 1 text
 - one
 - two
 - three
 
-# Page 2 |step,3500,2100,180,6
+# Page 2 ::step,3500,2100,180,6
 section 2 text
 
-# Page 3 |step,0,0,0,1
+# Page 3 ::step,0,0,0,1
 setion 3 text
 
--- js/impress_init.js
+++ js/impress_init.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var slides = [];
   $("#pages").children("h1").each(function(index){
     var onepage = $(this).nextUntil("h1");
-    var h1 = $(this).text().split("|");
+    var h1 = $(this).text().split("::");
     var meta = ["step", "1000", "1000", "0", "1"];
     if (h1 && h1[1]) {
       meta = h1[1].split(",");

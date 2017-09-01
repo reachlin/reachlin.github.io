3cb7c751e5afcce94659d340352bd30cb9ed012d
reachlin <reachlin@gmail.com>
2017-09-01 14:52:15 +0800

    animation

--- _posts/2017-08-31-impress-demo.md
+++ _posts/2017-08-31-impress-demo.md
@@ -1,29 +1,17 @@
 ---
 title: Impress Demo.
 layout: impress
-slug: slide-down
-classes: [text-center]
-
-data:
-  x: 0
-  y: 2000
-  scale: 3
-
-sections:
-  - The first page
-  - The Second page
-  - The third page
 ---
 
-# Page 1
+# Page 1 |step,850,3000,90,5
 section 1 text
 - one
 - two
 - three
 
-# Page 2
+# Page 2 |step,3500,2100,180,6
 section 2 text
 
-# Page 3
+# Page 3 |step,0,0,0,1
 setion 3 text
 
--- js/impress_init.js
+++ js/impress_init.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
   var slides = [];
   $("#pages").children("h1").each(function(index){
     var onepage = $(this).nextUntil("h1");
-    var div = $("<div class='step slide' data-x='1000' data-y='1000'></div>");
+    var h1 = $(this).text().split("|");
+    var meta = ["step", "1000", "1000", "0", "1"];
+    if (h1 && h1[1]) {
+      meta = h1[1].split(",");
+    }
+    var div = $("<div class='"+meta[0]+"' data-x='"+meta[1]+"' data-y='"+meta[2]+"' data-rotate='"+meta[3]+"' data-scale='"+meta[4]+"'>"+h1[0]+"</div>");
     div.append($(this)).append(onepage);
     $("#impress").append(div);
   });

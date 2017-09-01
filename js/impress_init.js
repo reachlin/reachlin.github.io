bfbcbccb7b305aafdf78f80fcdd76737e8ee8b28
reachlin <reachlin@gmail.com>
2017-09-01 10:35:17 +0800

    js

--- js/impress_init.js
+++ js/impress_init.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   impress().init();
   var slides = [];
-  $("#impress div").children("h1").each(function(h1){
-    var onepage = this.nextUntil("h1");
+  $("#impress div").children("h1").each(function(index){
+    var onepage = $(this).nextUntil("h1");
     var div = $("#impress").add("div");
     div.add(this);
     div.add(onepage);
